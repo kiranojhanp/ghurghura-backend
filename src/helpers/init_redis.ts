11ea6d5ef40db58bc0ffbfbe1173ab7d3e779d3c
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-04-29 12:45:44 +0545

    add docker

--- src/helpers/init_redis.ts
+++ src/helpers/init_redis.ts
@@ -1,7 +1,8 @@
-import * as redis from "redis"
 import createHttpError from "http-errors"
+import * as redis from "redis"
+const { REDIS_HOST } = process.env as { [key: string]: string }
 
-const client = redis.createClient({ url: "redis://127.0.0.1:6379" })
+const client = redis.createClient({ url: `${REDIS_HOST}` })
 client.connect()
 
 client.on("connect", () => {
